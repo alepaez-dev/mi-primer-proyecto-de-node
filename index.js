ac4485092113caaea0276c2d5b5f6d5b7fe834b6
Alejandra Paez <alepaezdev@gmail.com>
2023-05-31 19:42:37 -0600

    feat: ejercicio

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-console.log("Mi primer proyecto con Node");
+const argv = require("yargs-parser")(process.argv.slice(2))
+// console.log("argv", argv._)
 
+// @ts-ignore
+const nombre =  argv.nombre
+const apellido =  argv.apellido
+// console.log(`Nombre : ${nombre}, Apellido: ${apellido}`);
 /*
-* Leer documentacion.
-* Hacer un ejercicio con yargs-parser
+* Reutilizar el ejercicio de mandarle como argumento un nombre pero utilizando yargs parser, 
+* y buscarlo en el arreglo nombres.
+* si si lo encontramos bienvenido, si no fuera de aqui
+* La impresion de bienvenido tiene que ser en arcoiris
+* Y la impresion de Fuera de aqui, en rojo.
 */
