8115c87802314c2f140ce43c6f78177622ccdd38
Alejandra Paez <alepaezdev@gmail.com>
2023-06-01 20:05:06 -0600

    feat: ejercicio callbacks

--- callbacks.js
+++ callbacks.js
@@ -17,7 +17,7 @@ const saludar = () => {
 saludar
 
 // Ejecucion
-saludar()
+// saludar()
 
 
 // Callbacks
@@ -30,19 +30,88 @@ function irACasa(callback) {
   console.log("callback aqui", callback);
   console.log("Caminando hacia la casa...")
   callback()
-  callback()
-  callback()
-  callback()
 }
 
-// Callback con funcion anonima
-irACasa(() => console.log("Ya llegue a la casa ama"));
-// Callback declarada
-irACasa(avisar);
+// // Callback con funcion anonima
+// irACasa(() => console.log("Ya llegue a la casa ama"));
+// // Callback declarada
+// irACasa(avisar);
 
-
-const fs = {
-  readFile: () => { console.log("leyendo un archivo") }
+const koder = {
+  seDieronInformes: false,
+  seEntrevisto: false,
+  sePago: false,
+  seInscribio: false
 }
 
-fs.readFile();
+const darInformes = (koder, entrevistar) => {
+  // Tardo 3 segundos en darle informes al Koder
+  let error = null;
+  setTimeout(() => {
+    koder.seDieronInformes = true;
+    // ! -> null, undefined, false
+    if(!koder.seDieronInformes) {
+      error = "El koder necesita ser informado primero";
+    }
+    // Solucion
+    // callback -> entrevistamelo porque ya se le dieron informes
+    console.log("el koder ya se le dieron informes", koder);
+    entrevistar(error, koder);
+  }, 2000)
+}
+const seEntrevisto = (koderInformado, pagar) => {
+  let error = null
+  setTimeout(() => {
+    koderInformado.seEntrevisto = true;
+    if(!koderInformado.seEntrevisto) {
+      error = "El koder necesita ser entrevistado"
+    }
+    console.log("al koder ya se le entrevisto", koderInformado)
+    pagar(error, koderInformado);
+  }, 1000)
+}
+
+const sePago = (koderEntrevistado, inscribir) => {
+  let error = null;
+  setTimeout(() => {
+    koderEntrevistado.sePago = false;
+    if(!koderEntrevistado.sePago) {
+      error = "El koder necesita pagar"
+    }
+    console.log("El koder ya pago", koderEntrevistado);
+    inscribir(error, koderEntrevistado)
+  }, 2000)
+}
+
+const inscribir = (koderPagado) => {
+  setTimeout(() => {
+    koderPagado.seInscribio = true
+    console.log("El koder ya se inscribio!!!!", koderPagado);
+  }, 4000)
+}
+// Callbacks anonimas
+darInformes(koder, (error, koderInformado) => {
+  if(error) {
+    console.log(error)
+    return;
+  }
+  // Koder ya esta informado
+  seEntrevisto(koderInformado, (error, koderEntrevistado) => {
+    if(error) {
+      console.log(error)
+      return;
+    }
+    // Koder ya esta entrevistado
+    sePago(koderEntrevistado, (error, koderPagado) => {
+      if(error) {
+        console.log(error)
+        return;
+      }
+      // Koder ya pago
+      inscribir(koderPagado);
+    })
+  })
+});
+
+
+
